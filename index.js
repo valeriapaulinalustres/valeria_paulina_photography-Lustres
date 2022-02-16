d1c9081df205f9638199f459de7f8dcaf80ca3ff
valeriapaulinalustres <valeriapaulinalustres@yahoo.com>
2022-02-16 07:24:44 -0300

    funcionan borrar ítem y vaciar carro

--- index.js
+++ index.js
@@ -3,13 +3,12 @@
 //muestra listado de sesiones en el carrito
 
 const showProductCarts = () => {
-    const divCart = document.getElementById("productsOnCart")
-    let htmlListProducts = ""
+    const divCart = document.getElementById("productsOnCart");
+    let htmlListProducts = "";
 
-
-    CART.forEach(product => {
+    CART.forEach((product) => {
         htmlListProducts += `
-            <div id="cartItems" class="carrito-info" style="border: 1px solid #98CBCB">
+            <div id="cartItems-${product.id}" class="carrito-info" style="border: 1px solid #98CBCB">
                 <img src="${product.img}" width="100"></img><br>
                 <b>${product.name}</b><br>
                 <i>Cantidad: ${product.quantity}</i>
@@ -17,155 +16,117 @@ const showProductCarts = () => {
                 <p>Total: $ ${product.total}</p>
                 <button class="contactos deleteItem" id="${product.id}">❌</button>
             </div>
-        `
+        `;
     });
 
-
     divCart.innerHTML = htmlListProducts;
-
 
     //para borrar items del carrito
 
     let botones = document.getElementsByClassName("deleteItem");
     for (const boton of botones) {
         boton.onclick = (event) => {
-            //alert(boton.id);
-
             const id = parseInt(event.target.id);
-            //alert("funciona" + id);
-            //deleteCartItem(boton.onclick);
-
-            //function deleteCartItem() {
-
-            let cartItems = document.getElementById("cartItems")
-
-            function borrarItem(id) {
-                if (id === id) {
-
-                    //
-                    //if (this.quantity>1) {this.quantity=this.quantity-1
-
-                    //else {cartItems.remove()
-
-
-                    //
-
-
-                    cartItems.remove()
-
-                }
-            }
-
-            borrarItem(id)
-            //localStorage.removeItem(id)
-
-        }
-
-
+            let cartItems = document.getElementById(`cartItems-${id}`);
+            cartItems.remove();
+            //busco id del producto para capturar su índice
+            const capturarIndiceDelObjetoABorrar = CART.findIndex(
+                (product) => product.id === id
+            );
+            CART.splice(capturarIndiceDelObjetoABorrar, 1);
+        };
     }
-
     registerClickEvent();
 };
 
 
-
 //muestra botones para seleccionar filtrado
 
-const showProducts = (category = 'all') => {
-    const divProducts = document.getElementById("products")
-    let htmlListProducts = ""
+const showProducts = (category = "all") => {
+    const divProducts = document.getElementById("products");
+    let htmlListProducts = "";
 
-    let products = []
+    let products = [];
 
-    if (category == 'cheap') products = PRODUCTS.filter(p => p.price < 2000)
-    else if (category == 'expensive') products = PRODUCTS.filter(p => p.price >= 6000)
-    else products = PRODUCTS
+    if (category == "cheap") products = PRODUCTS.filter((p) => p.price < 2000);
+    else if (category == "expensive")
+        products = PRODUCTS.filter((p) => p.price >= 6000);
+    else products = PRODUCTS;
+    
 
     //muestra listado de sesiones ofrecidas
-    products.forEach(product => {
+    products.forEach((product) => {
         htmlListProducts += `
             <div class="container-js" style="border: 2px solid white">
                 <img src="${product.img}" height="50"></img><br>
                 <b>${product.name}</b>
                 <p>$ ${product.price}</p>
-
                 <button class="addCart contactos" id="p-${product.id}">Comprar 🛒</button>
             </div>
-        `
-    })
-
-    divProducts.innerHTML = htmlListProducts
-
-    registerClickEvent()
-}
+        `;
+    });
+    divProducts.innerHTML = htmlListProducts;
+    registerClickEvent();
+};
 
 //evento clic en botón comprar
 
 const registerClickEvent = () => {
-
-    const btnAddCarts = document.getElementsByClassName("addCart")
+    const btnAddCarts = document.getElementsByClassName("addCart");
     for (const btn of btnAddCarts) {
-        btn.onclick = addCart
+        btn.onclick = addCart;
     }
-
-}
+};
 
 //muestra lo que se va agregando al carro
 
 const addCart = (event) => {
-    const productId = parseInt(event.target.id.split("-")[1])
-
-    const product = PRODUCTS.find(p => p.id == productId)
-    const productInCart = CART.find(p => p.id == productId)
-
-    if (productInCart) productInCart.add()
+    const productId = parseInt(event.target.id.split("-")[1]);
+    const product = PRODUCTS.find((p) => p.id == productId);
+    const productInCart = CART.find((p) => p.id == productId);
+    if (productInCart) productInCart.add();
     else {
-        const productCart = new ProductCart(product)
-        CART.push(productCart)
+        const productCart = new ProductCart(product);
+        CART.push(productCart);
         //actualiza localStorage
-        updateCache()
+        updateCache();
         //
     }
-
-    showProductCarts()
-    CalculateTotalCart()
-
-}
+    showProductCarts();
+    CalculateTotalCart();
+};
 
 //suma el total de la compra
 
 const CalculateTotalCart = () => {
-    let suma = 0
-    CART.forEach(p => suma += p.total)
+    let suma = 0;
+    CART.forEach((p) => (suma += p.total));
+    const elemntTotal = document.getElementById("totalCart");
+    elemntTotal.innerHTML = suma;
+};
 
-    const elemntTotal = document.getElementById("totalCart")
-    elemntTotal.innerHTML = suma
-}
-
-CalculateTotalCart()
-showProducts()
+CalculateTotalCart();
+showProducts();
 
 document.getElementById("btnShowProductAll").onclick = () => {
-    showProducts('all')
-}
+    showProducts("all");
+};
 document.getElementById("btnShowProductCheap").onclick = () => {
-    showProducts('cheap')
-}
+    showProducts("cheap");
+};
 document.getElementById("btnShowProductExpensive").onclick = () => {
-    showProducts('expensive')
-}
+    showProducts("expensive");
+};
 
-// Vaciar el carrito 
-let cartItems = document.getElementById("cartItems")
-let botonVaciarTodo = document.getElementById("vaciarTodo")
-botonVaciarTodo.addEventListener('click', borrarNodoCartItems)
+// Vaciar el carrito
+let cartItems = document.getElementById("cartItems");
+let botonVaciarTodo = document.getElementById("vaciarTodo");
+botonVaciarTodo.addEventListener("click", borrarNodoCartItems);
 
 function borrarNodoCartItems() {
-    alert("funciona")
-    cartItems.remove()
-
-    let productsOnCart = document.getElementById("productsOnCart")
-    productsOnCart.innerHTML = "<p>Carrito Vacío</p>"
+    let productsOnCart = document.getElementById("productsOnCart");
+    productsOnCart.innerHTML = "<p>Carrito Vacío</p>";
+    CART.splice(0, CART.length);
 }
 
 //localStorage.clear()
