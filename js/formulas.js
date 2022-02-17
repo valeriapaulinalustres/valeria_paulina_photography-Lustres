7253a60878d0bf98e914f469a1da2350bbbe5d13
valeriapaulinalustres <valeriapaulinalustres@yahoo.com>
2022-02-17 19:06:10 -0300

    separo del proyecto de JavaScript

--- js/formulas.js
+++
@@ -1,9 +0,0 @@
-const updateCache = () => {
-    const cartJSON = JSON.stringify(CART)
-    localStorage.setItem("CART", cartJSON)
-}
-
-
-//saca del storage, pasa de string a array y muestra por consola:
-let storageItems = JSON.parse(localStorage.getItem("CART"));
-console.log(storageItems);
